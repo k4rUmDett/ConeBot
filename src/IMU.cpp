fa9a2a619d2640460ae87fde267b9ca358a1fcce
k4rUm <pongpang.dett@gmail.com>
2024-11-26 23:40:48 -0800

    Initial commit

---
+++ src/IMU.cpp
@@ -0,0 +1,78 @@
+#include "IMU.h"
+#include <Arduino.h>
+
+IMU::IMU(uint8_t address)
+    : bno(55, address), address(address), calibrated(false), pitch(0), angularVelocity(0) {}
+
+bool IMU::begin() {
+    // Initialize the BNO055 with the correct operation mode
+    if (!bno.begin((adafruit_bno055_opmode_t)0x0C)) {  // Use the enum or its equivalent directly
+        Serial.println("Error initializing BNO055!");
+        return false;
+    }
+    delay(1000); // Allow sensor to stabilize
+    bno.setExtCrystalUse(true);
+    return true;
+}
+
+void IMU::calibrate() {
+    Serial.println("Calibrating IMU...");
+    uint8_t sys, gyro, accel, mag;
+    do {
+        bno.getCalibration(&sys, &gyro, &accel, &mag);
+        Serial.print("Sys:");
+        Serial.print(sys, DEC);
+        Serial.print(" Gyro:");
+        Serial.print(gyro, DEC);
+        Serial.print(" Accel:");
+        Serial.print(accel, DEC);
+        Serial.print(" Mag:");
+        Serial.println(mag, DEC);
+        delay(100);
+    } while (sys < 3 || gyro < 3 || accel < 3 || mag < 3);
+    calibrated = true;
+    Serial.println("IMU Calibrated!");
+}
+
+void IMU::update() {
+    if (!calibrated) {
+        Serial.println("IMU not calibrated yet!");
+        return;
+    }
+    computeState();
+}
+
+float IMU::getPitch() {
+    return pitch;
+}
+
+float IMU::getAngularVelocity() {
+    return angularVelocity;
+}
+
+bool IMU::isCalibrated() const {
+    return calibrated;
+}
+
+void IMU::printCalibrationStatus() {
+    uint8_t sys, gyro, accel, mag;
+    bno.getCalibration(&sys, &gyro, &accel, &mag);
+    Serial.print("Sys:");
+    Serial.print(sys, DEC);
+    Serial.print(" Gyro:");
+    Serial.print(gyro, DEC);
+    Serial.print(" Accel:");
+    Serial.print(accel, DEC);
+    Serial.print(" Mag:");
+    Serial.println(mag, DEC);
+}
+
+void IMU::computeState() {
+    // Get Euler angles (yaw, pitch, roll)
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    pitch = euler.z(); // Forward/backward tilt angle
+
+    // Get angular velocity from gyroscope
+    imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    angularVelocity = gyro.y(); // Pitch rate of change
+}
---
+++ src/IMU.h
@@ -0,0 +1,34 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BNO055.h>
+
+
+class IMU {
+public:
+    IMU(uint8_t address = BNO055_ADDRESS_A);
+
+    bool begin();
+    void calibrate();
+    void update();
+
+    float getPitch(); // Forward/backward tilt angle
+    float getAngularVelocity(); // Angular velocity (pitch rate)
+
+    bool isCalibrated() const;
+    void printCalibrationStatus();
+
+private:
+    Adafruit_BNO055 bno;
+    uint8_t address;
+    bool calibrated;
+
+    float pitch;              // Forward/backward tilt
+    float angularVelocity;    // Angular velocity (pitch rate)
+
+    void computeState();
+};
+
+#endif
